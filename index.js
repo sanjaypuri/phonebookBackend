b05a8470bab472776182a0d37aad5a0549e2bf92
Sanjay Puri <39893694+sanjaypuri@users.noreply.github.com>
2023-09-28 09:13:31 +0530

    PUT method added for CORS

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors(
   {
     origin:["http://localhost:3000", "https://phonebookfrontend.vercel.app"],
-    methods:["GET", "POST", "DELETE"],
+    methods:["GET", "POST", "DELETE", "PUT"],
     credentials: true
   }
 ));
